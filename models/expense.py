2262caf8d2d10e116d6e38ce39d9a527b046de1b
Kevin <khanglqse@gmail.com>
2024-09-12 23:24:07 +0700

    update add expense

--- models/expense.py
+++ models/expense.py
@@ -8,18 +8,75 @@ class Expense:
         self.category = category
         self.description = description
         self.date = date
-    def getAllExpense(user_id, page, size):
+    def get_expenses(user_id, page, size, filters, sort_by, sort_order):
         conn = sqlite3.connect('pig_save.db')
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM expenses WHERE user_id = ? LIMIT ? OFFSET ?', (user_id, size, (page -1) * size))
+        
+        query = 'SELECT * FROM expenses WHERE user_id = ?'
+        params = [user_id]
+        
+        if filters.get('category'):
+            query += ' AND category = ?'
+            params.append(filters['category'])
+        
+        if filters.get('start_date'):
+            query += ' AND date >= ?'
+            params.append(filters['start_date'])
+
+        if filters.get('end_date'):
+            query += ' AND date <= ?'
+            params.append(filters['end_date'])
+
+        if filters.get('min_amount'):
+            query += ' AND amount >= ?'
+            params.append(filters['min_amount'])
+
+        if filters.get('max_amount'):
+            query += ' AND amount <= ?'
+            params.append(filters['max_amount'])
+        
+        params.extend([size, (page - 1) * size])
+        query += f' ORDER BY {sort_by} {sort_order} LIMIT ? OFFSET ?'
+        cursor.execute(query, params)
         expenses = cursor.fetchall()
-        total_expenses = conn.execute('SELECT COUNT(*) FROM expenses').fetchone()[0]
+        
+        query_count = 'SELECT COUNT(*) FROM expenses WHERE user_id = ?'
+        count_params = [user_id]
+        
+        if filters.get('category'):
+            query_count += ' AND category = ?'
+            count_params.append(filters['category'])
+        
+        if filters.get('start_date'):
+            query_count += ' AND date >= ?'
+            count_params.append(filters['start_date'])
+
+        if filters.get('end_date'):
+            query_count += ' AND date <= ?'
+            count_params.append(filters['end_date'])
+
+        if filters.get('min_amount'):
+            query_count += ' AND amount >= ?'
+            count_params.append(filters['min_amount'])
+
+        if filters.get('max_amount'):
+            query_count += ' AND amount <= ?'
+            count_params.append(filters['max_amount'])
+        total_expenses = conn.execute(query_count, count_params).fetchone()[0]
         conn.close()
+        
         return expenses, total_expenses
-    def add_expense(user_id, description, amount, date):
-        conn = sqlite3.connect('pig_save.db')  # Update with your actual database path
+    def add_expense(user_id, amount, category, description, date):
+        conn = sqlite3.connect('pig_save.db')
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO expenses (user_id, description, amount, date) VALUES (?, ?, ?, ?)",
-                    (description, amount, date))
+        cursor.execute("INSERT INTO expenses (user_id, amount, category, description, date) VALUES (?, ?, ?, ?, ?)",
+                       (user_id, amount, category, description, date))
+        conn.commit()
+        conn.close()
+    def update_expense(expense_id, amount, category, description, date):
+        conn = sqlite3.connect('pig_save.db')
+        cursor = conn.cursor()
+        cursor.execute("UPDATE expenses SET  amount = ?, category = ?, description = ?, date = ? WHERE id = ?",
+                       (amount, category, description, date, expense_id))
         conn.commit()
         conn.close()
--- routes.py
+++ routes.py
@@ -69,10 +69,14 @@ def expenses():
     user_id = current_user.id
     page = request.args.get('page', 1, type=int)
     size = request.args.get('size', 5, type=int)
-    expenses,total_expenses  = Expense.getAllExpense(user_id, page, size)
+    sort_by = request.args.get('sort_by', 'date')  # Default to sorting by date
+    sort_order = request.args.get('sort_order', 'asc')  # Default to ascending order
+
+    filters = {key: request.args.get(key) for key in ['category', 'start_date', 'end_date', 'min_amount', 'max_amount']}
+    expenses, total_expenses = Expense.get_expenses(user_id, page, size, filters, sort_by, sort_order)
+
     total_pages = math.ceil(total_expenses / size)
     max_pages_displayed = 4
-    math
     start_page = max(1, page - 2)
     end_page = min(total_pages, start_page + max_pages_displayed - 1)
 
@@ -81,19 +85,32 @@ def expenses():
 
     return render_template('expense/list.html', 
                            expenses=expenses,
-                           page = page,
-                           total_pages = total_pages,
-                           end_page= end_page,
-                           start_page = start_page,
-                           size = size)
+                           page=page,
+                           total_pages=total_pages,
+                           end_page=end_page,
+                           start_page=start_page,
+                           filters=filters,
+                           size=size,
+                           sort_by=sort_by,
+                           sort_order=sort_order)
 
 @routes.route('/add_expense', methods=['POST'])
 def add_expense():
-    description = request.form['description']
-    amount = request.form['amount']
-    date = request.form['date']
-    Expense.add_expense(current_user.id, description, amount, date)
-    return redirect(url_for('main.expenses'))
+    amount, category, description, date = [request.form[key] for key in ['amount', 'category', 'description', 'date']]
+    page = request.form.get('page', 1, type=int)
+    size = request.form.get('size', 5, type=int)
+    filters = {key: request.form.get(key) for key in ['category', 'start_date', 'end_date', 'min_amount', 'max_amount']}
+    Expense.add_expense(current_user.id, amount, category, description, date)
+    return redirect(url_for('main.expenses', page=page, size=size, **filters))
+@routes.route('/edit_expense', methods=['POST'])
+def edit_expense():
+    expense_id = request.form['expenseId']
+    amount, category, description, date = [request.form[key] for key in ['amount', 'category', 'description', 'date']]
+    filters = {key: request.form.get(key) for key in ['category_filter', 'start_date', 'end_date', 'min_amount', 'max_amount']}
+    page = request.form.get('page', 1, type=int)
+    size = request.form.get('size', 5, type=int)
+    Expense.update_expense(expense_id, amount, category, description, date)
+    return redirect(url_for('main.expenses', page=page, size=size, **filters))
 
 @routes.route('/logout')
 @login_required
