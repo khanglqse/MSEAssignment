8cae3741796ac4470d34f1635b50f137e74cc4b5
Kevin <khanglqse@gmail.com>
2024-09-15 14:12:00 +0700

    add filter + update db column

--- core/generator.py
+++ core/generator.py
@@ -6,17 +6,17 @@ conn = sqlite3.connect('../pig_save.db')
 cursor = conn.cursor()
 
 def generate_random_data():
-    categories = ['Food', 'Travel', 'Utilities', 'Entertainment', 'Healthcare', 'Others']
+    categories = ['Food', 'Transport', 'Utilities', 'Entertainment', 'Others']
     descriptions = ['Groceries', 'Bus ticket', 'Electric bill', 'Movie ticket', 'Doctor visit', 'Miscellaneous']
     amount = random.randrange(1000, 50000001, 1000)
     category = random.choice(categories)
     description = random.choice(descriptions)
     date = (datetime.now() - timedelta(days=random.randint(1, 365))).strftime('%Y-%m-%d') 
     print('called!!')
-    return (2, amount, category, description, date)
+    return (1, amount, category, description, date)
 
 
-for _ in range(200):
+for _ in range(400):
     data = generate_random_data()
     cursor.execute('''
         INSERT INTO expenses (user_id, amount, category, description, date)
