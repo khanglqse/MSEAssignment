7c3d2ef13ecf79de94a64eec3871b8063b74e7a4
Khang Luong <khanglqse@gmail.com>
2024-09-19 10:56:13 +0700

    update host

--- main.py
+++ main.py
@@ -51,5 +51,4 @@ app.register_blueprint(main_routes)
 app.register_blueprint(auth_routes)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
